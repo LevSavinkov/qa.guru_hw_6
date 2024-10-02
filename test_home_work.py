c8b6740ba86f1195e376205ed730573478856289
Lev Savinkov <lsavinkov@alfabank.ru>
2024-10-02 09:13:08 +0300

    did home_work

--- test_home_work.py
+++ test_home_work.py
@@ -29,8 +29,6 @@ def test_dark_theme_by_time_and_user_choice():
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
     
-    is_dark_theme = None
-    
     if 6 < current_time.hour < 22 and dark_theme_enabled_by_user is None:
         is_dark_theme = False
     elif dark_theme_enabled_by_user is False:
